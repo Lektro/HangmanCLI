22d9c1d145239145b9212990689945e5eb920b72
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-08-12 15:25:18 +0200

    Hangman basic done

--- src/main/java/be/vdab/app/AppMain.java
+++ src/main/java/be/vdab/app/AppMain.java
@@ -1,10 +1,12 @@
 package be.vdab.app;
 
+import be.vdab.utility.*;
+
 public class AppMain {
     public static void main(String[] args) {
         System.out.println("Hangman Game with Word Collections.");
-        //HangmanGame hangmanGame = new HangmanGame();
-        //hangmanGame.newGame();
-        //hangmanGame.play();
+        HangmanGame hangmanGame = new HangmanGame();
+        hangmanGame.newGame();
+        hangmanGame.play();
     }
 }
--- src/main/java/be/vdab/utility/ReadRandomWord.java
+++
@@ -1,4 +0,0 @@
-package be.vdab.utility;
-
-public class ReadRandomWord {
-}
